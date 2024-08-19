59e822ba56d8b49ae70482089d05ddeb9a9b3adb
Md Mehedi Hasan Rakib <mhrakib708@gmail.com>
2024-08-19 12:43:41 +0600

    Upadate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 import BadgeVariables from './component/BadgeVariables';
 
+function Layout() {
+  return (
+    <>
+    <div className="container">
+    <header>Header</header>
+    <main>Main</main>
+    <aside>Aside</aside>
+    <footer>Footer</footer>
+    </div>
+    </>
+  );
+}
+
+
+
 function App() {
   return (
     <>
     <BadgeVariables />
+    <Layout />
     </>
   );
 }
